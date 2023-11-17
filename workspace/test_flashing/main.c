d206e83cf61650e384806b6f8738dd4141b9ba6e
Chandran Goodchild <chandran.goodchild@alamance.ai>
2023-11-17 09:50:41 +0530

    Works with button handler

--- workspace/test_flashing/main.c
+++ workspace/test_flashing/main.c
@@ -1,4 +1,18 @@
+#include "nrf_sdh.h"
 #include "boards.h"
+#include "app_timer.h"
+#include "app_button.h"
+
+
+static void button_handler(uint8_t pin, uint8_t action) {
+    if (pin == BSP_BUTTON_0) {
+        if (action == APP_BUTTON_PUSH) {
+            bsp_board_led_on(BSP_BOARD_LED_1);
+        } else if (action == APP_BUTTON_RELEASE) {
+            bsp_board_led_off(BSP_BOARD_LED_1);
+        }
+    }
+}
 
 
 /**@brief Function for application main entry.
@@ -6,19 +20,21 @@
 int main(void)
 {
     // Initialize.
-    bsp_board_init(BSP_INIT_LEDS | BSP_INIT_BUTTONS);
-
-    while(true)
+    bsp_board_init(BSP_INIT_LEDS);
+    app_timer_init();
+    nrf_sdh_enable_request();
+    static app_button_cfg_t buttons[] = {
     {
-        if (bsp_board_button_state_get(BSP_BUTTON_0))
-        {
-            bsp_board_led_on(BSP_BOARD_LED_2);
-        }
-        else
-        {
-            bsp_board_led_off(BSP_BOARD_LED_2);
-        }
+        BSP_BUTTON_0,
+        false,
+        BUTTON_PULL,
+        button_handler
     }
+    };
+
+    app_button_init(buttons, ARRAY_SIZE(buttons), APP_TIMER_TICKS(50));
+    app_button_enable();
+
 }
 
 
